462f63939245215cefbfdbd8c81d88ba486c3f38
FrostGod <divesh.chowdary@gmail.com>
2022-06-13 15:17:34 +0530

    setup config for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -33,7 +33,7 @@ weatherForm.addEventListener('submit', (event) => {
     console.log(location);
     messageOne.textContent = "Loading...";
 
-    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+    fetch("/weather?address=" + location).then((response) => {
     response.json().then((data)=> {
         if(data.err){
             messageOne.textContent = data.err;
